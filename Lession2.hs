2acfaab6c7dbdcbcbbf49b7c353ea40ab57228e0
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-17 13:32:07 +0000

    Initial commit

---
+++ Lession2.hs
@@ -0,0 +1,10 @@
+{- Question 1
+ Add the type signatures for the functions below and then remove the comments and try to compile.
+ (Use the types presented in the lecture.) -}
+
+f1 :: Double -> Double -> Double -> Double
+f1 x y z = x ** (y / z)
+-- ** là phép toán số mũ kaka
+
+f2 :: Double -> Double -> Double -> Double
+f2 x y z = sqrt (x / y - z)
