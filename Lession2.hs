ec1192ecf1cf5de09427d2e48af745d7fd93eb8f
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-18 15:06:04 +0000

    Update

--- Lession2.hs
+++ Lession2.hs
@@ -18,3 +18,23 @@ f4 :: String -> String -> String -> Bool
 f4 x y z = x == (y ++ z)
 {-Cảnh báo Việc sử dụng lặp lại toán tử ++ trong các danh sách dài (thậm chí nếu bạn thêm một danh sách đơn lẻ vào một danh sách, chẳng hạn: [1,2,3] ++ [4]), buộc Haskell phải duyệt qua toàn bộ danh sách ở phía bên trái của ++. Do đó, việc đặt thứ gì đó ở cuối danh sách dài năm mươi triệu mục nhập sẽ mất một khoảng thời gian! Tuy nhiên, đặt một cái gì đó ở đầu danh sách bằng toán tử khuyết điểm : là ngay lập tức!-}
 
+{- Question 2
+ Q: Why should we define type signatures of functions? How can they help you? How can they help others?
+ A: Because, need to message to the whole world that such function exists.
+    Here is its name and here are the types it works with.
+    Don't work if type signatures is wrong.
+    Bởi vì, cần thông báo với toàn thế giới rằng chức năng đó tồn tại.
+    Đây là tên của nó và đây là những loại mà nó hoạt động.
+    Nó sẽ không hoạt động nếu định nghĩa nó sai.
+   Question 3 
+ Q: Why should you define type signatures for variables? How can they help you?
+ A: Haskell's variables are immutable.
+    Haskell's concept of variable is different. Haskell has variables, but in the mathematical sense. 
+   Question 4
+ Q: Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
+ A: I tried googling but don't have a answer for it until now...
+ 
+   Question 5
+ Q: Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner most elements?-}
+Sentence :: [[[ Char ]]]
+Sentence = [[" Li " , " onel "], [" Mes " , " si "]]
